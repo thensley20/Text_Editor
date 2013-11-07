6573d5e2a9a9b1081f66e705dd1d2e8d7309fd99
nrandall92 <nrandall@mail.umw.edu>
2013-11-06 22:08:56 -0500

    Text Editor project

---
+++ main.java
@@ -0,0 +1,18 @@
+//main program
+
+package Text_Editor;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+//runs the text editor program
+public class main
+{
+	public static void main (String[] args)
+	{
+		TELayout textEditor = new TELayout();
+		textEditor.doLayout();
+	}
+}
